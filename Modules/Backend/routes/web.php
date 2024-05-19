8c799e12faf087df481d1530cf03fab43c6a7974
Mojahid islam <raofahmedmojahid@gmail.com>
2024-05-19 23:42:19 +0600

    add backend admin template base

--- Modules/Backend/routes/web.php
+++ Modules/Backend/routes/web.php
@@ -14,6 +14,15 @@ use Modules\Backend\Http\Controllers\BackendController;
 |
 */
 
-Route::group([], function () {
-    Route::resource('backend', BackendController::class)->names('backend');
+// Route::prefix('admin')->group([], function () {
+//     Route::get('/', function () {
+//         return view('backend::index');
+//     })->name('index');
+//     // Route::resource('backend', BackendController::class)->names('backend');
+// });
+
+Route::prefix("admin")->name('admin.')->group(function () {
+    Route::get('/', function () {
+        return view("backend::layouts.base.master.index");
+    })->name('index');
 });
